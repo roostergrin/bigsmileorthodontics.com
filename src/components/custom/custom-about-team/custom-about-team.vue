34ef30c448aeac596ab689407a65cbc4fb405ffc
luketevers <luketevers@gmail.com>
2020-01-03 13:15:46 -0800

    creates(custom-about-team): creates on all screens and devices

---
+++ src/components/custom/custom-about-team/custom-about-team.vue
@@ -0,0 +1,39 @@
+<template lang="pug" src="./custom-about-team.pug"></template>
+
+<script>
+import { onScroll } from '@/resources/mixins'
+
+export default {
+  data () {
+    return {
+      containerActive: false,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.2]
+      }
+    }
+  },
+  props: {
+    content: {
+      Type: Object,
+      default: () => ({})
+    },
+    tiles: {
+      Type: String,
+      default: 'three',
+      validator: function (str) {
+        return ['two', 'three', 'four'].indexOf(str) !== -1
+      }
+    }
+  },
+  mixins: [onScroll],
+  methods: {
+    onContainerScroll ({going, direction}) {
+      if (going === 'in' && direction === 'top') {
+        this.containerActive = true
+      }
+    }
+  }
+}
+</script>
