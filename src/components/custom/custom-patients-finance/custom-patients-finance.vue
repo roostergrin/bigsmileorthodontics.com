adbc33892a20056c46ef303a7cf7246c31023137
Spencer Alexander <slalexander92@gmail.com>
2020-01-03 15:54:42 -0800

    create(patient): create patient page, components

---
+++ src/components/custom/custom-patients-finance/custom-patients-finance.vue
@@ -0,0 +1,22 @@
+<template lang="pug" src="./custom-patients-finance.pug"></template>
+
+<script>
+export default {
+  props: {
+    props: {
+      type: Object
+    }
+  },
+  computed: {
+    backgroundImg () {
+
+      const screenIsMobile = window.innerWidth < 1024
+      if (screenIsMobile) {
+        return this.props.mobile_background_img
+      } else {
+        return this.props.background_img
+      }
+    }
+  }
+}
+</script>
