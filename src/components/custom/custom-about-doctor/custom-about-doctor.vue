c2f96cc056e20e83cb95aabbfbf0ad5c304eb2be
luketevers <luketevers@gmail.com>
2020-01-03 13:14:49 -0800

    creates(custom-about-doctor): creates on all screens and devices

---
+++ src/components/custom/custom-about-doctor/custom-about-doctor.vue
@@ -0,0 +1,12 @@
+<template lang="pug" src="./custom-about-doctor.pug"></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
