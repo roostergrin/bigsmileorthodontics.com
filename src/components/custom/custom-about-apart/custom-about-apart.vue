3b87dcf5f15a09d0c7adfc12f4608783304652fd
luketevers <luketevers@gmail.com>
2020-01-03 13:13:43 -0800

    creates(custom-about-apart): creates on all screens and devices

---
+++ src/components/custom/custom-about-apart/custom-about-apart.vue
@@ -0,0 +1,12 @@
+<template lang="pug" src="./custom-about-apart.pug"></template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
