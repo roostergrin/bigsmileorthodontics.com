f5f35b121a4127551369ba343e5b04cab9d6f48f
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:52:29 -0800

    creates(custom-treatments-braces): add content and styling

---
+++ src/components/custom/custom-treatments-braces/custom-treatments-braces.vue
@@ -0,0 +1,29 @@
+<template lang="pug" src="./custom-treatments-braces.pug"></template>
+
+<script>
+import { onScroll } from '@/resources/mixins'
+
+export default {
+  props: {
+    props: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeTab: 0,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.4]
+      }
+    }
+  },
+  mixins: [onScroll],
+  methods: {
+    setTab (i) {
+      this.activeTab = i
+    }
+  }
+}
+</script>
