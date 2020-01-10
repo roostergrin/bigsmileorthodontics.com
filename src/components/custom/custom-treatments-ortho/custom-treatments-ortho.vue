f65dca7b778422274e9a6ef33d735bd5fb4d21f1
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:52:58 -0800

    updates(custom-treatments-ortho): add content and styling

---
+++ src/components/custom/custom-treatments-ortho/custom-treatments-ortho.vue
@@ -0,0 +1,23 @@
+<template lang="pug" src="./custom-treatments-ortho.pug"></template>
+
+<script>
+import { onScroll } from '@/resources/mixins'
+
+export default {
+  mixins: [onScroll],
+  props: {
+    props: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.4]
+      }
+    }
+  }
+}
+</script>
