6deff184269947858bb458afadeec245ba756258
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:51:08 -0800

    updates(custom-patients-finance): add title animation

--- src/components/custom/custom-patients-finance/custom-patients-finance.vue
+++ src/components/custom/custom-patients-finance/custom-patients-finance.vue
@@ -1,12 +1,24 @@
 <template lang="pug" src="./custom-patients-finance.pug"></template>
 
 <script>
+import { onScroll } from '@/resources/mixins'
+
 export default {
   props: {
     props: {
       type: Object
     }
   },
+  data () {
+    return {
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.5]
+      }
+    }
+  },
+  mixins: [onScroll],
   computed: {
     backgroundImg () {
       const screenIsMobile = window.innerWidth < 1024
