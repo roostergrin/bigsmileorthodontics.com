cd78181db017575853e9d31e2296288436fd5298
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:46:30 -0800

    creates(custom-contact-page): add contact page section

---
+++ src/components/custom/custom-contact-page/custom-contact-page.vue
@@ -0,0 +1,19 @@
+<template lang="pug" src="./custom-contact-page.pug"></template>
+
+<script>
+import FormContact from 'components/form/form-contact/form-contact'
+
+export default {
+  props: {
+    contact: {
+      type: [Object, Array]
+    },
+    props: {
+      type: [Object, Array]
+    }
+  },
+  components: {
+    FormContact
+  }
+}
+</script>
