321632767031fc542239f997289e97bdef2ae6b7
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:56:53 -0800

    updates(contact): add page sections

--- src/pages/contact/contact.vue
+++ src/pages/contact/contact.vue
@@ -1,14 +1,22 @@
 <template lang="pug" src="./contact.pug"></template>
 
 <script>
+import HeroPhoto from '@/components/block/block-hero-photo/block-hero-photo'
+import CustomContactPage from 'components/custom/custom-contact-page/custom-contact-page'
+
 export default {
   name: 'Contact Us',
   computed: {
     props () {
       return this.$store.state.pages['contact-us']
+    },
+    contact () {
+      return this.$store.state.app['contact-information']
     }
   },
   components: {
+    HeroPhoto,
+    CustomContactPage
   }
 }
 </script>
