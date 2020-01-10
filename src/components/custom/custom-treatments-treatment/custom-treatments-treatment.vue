efbb5712f91cfb8a09d7aaddd16ab9c793a81e7b
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:53:13 -0800

    creates(custom-treatments-treatment): add content and styling

---
+++ src/components/custom/custom-treatments-treatment/custom-treatments-treatment.vue
@@ -0,0 +1,31 @@
+<template lang="pug" src="./custom-treatments-treatment.pug"></template>
+
+<script>
+import { onScroll } from '@/resources/mixins'
+
+export default {
+  props: {
+    props: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.4]
+      }
+    }
+  },
+  mixins: [onScroll],
+  methods: {
+    onWaypoint ({ going, direction, el }) {
+      if (going === 'in' && direction === 'top') {
+        const htmlItem = el.children[0]
+        htmlItem.classList.add('custom-treatments-treatment__title--active')
+      }
+    }
+  }
+}
+</script>
