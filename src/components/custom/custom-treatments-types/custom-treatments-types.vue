ae2e39b1903ed16cb57f89e73b80b0d04abc97cb
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:53:30 -0800

    updates(custom-treatments-types): add content and styling

---
+++ src/components/custom/custom-treatments-types/custom-treatments-types.vue
@@ -0,0 +1,29 @@
+<template lang="pug" src="./custom-treatments-types.pug"></template>
+
+<script>
+import { onScroll } from '@/resources/mixins'
+
+export default {
+  props: {
+    props: {
+      type: [Object, Array]
+    }
+  },
+  data () {
+    return {
+      activeType: 0,
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.4]
+      }
+    }
+  },
+  mixins: [onScroll],
+  methods: {
+    setType (i) {
+      this.activeType = i
+    }
+  }
+}
+</script>
