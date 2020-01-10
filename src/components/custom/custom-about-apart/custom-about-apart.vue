168ae42211883ef52feebff8c00400cd6c14b8af
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:44:25 -0800

    updates(custom-about-apart): add title animation

--- src/components/custom/custom-about-apart/custom-about-apart.vue
+++ src/components/custom/custom-about-apart/custom-about-apart.vue
@@ -1,12 +1,24 @@
 <template lang="pug" src="./custom-about-apart.pug"></template>
 
 <script>
+import { onScroll } from '@/resources/mixins'
+
 export default {
   props: {
     content: {
       type: Object,
       default: () => ({})
     }
-  }
+  },
+  data () {
+    return {
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.5]
+      }
+    }
+  },
+  mixins: [onScroll]
 }
 </script>
