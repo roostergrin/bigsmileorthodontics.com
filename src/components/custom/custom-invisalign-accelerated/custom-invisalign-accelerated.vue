4bd70f5016ef790f933d9a6c606b7870aeb2a4a9
Carlynn <carlynn.espinoza@gmail.com>
2020-01-10 10:49:46 -0800

    updates(custom-invisalign-accelerated): add title animation

--- src/components/custom/custom-invisalign-accelerated/custom-invisalign-accelerated.vue
+++ src/components/custom/custom-invisalign-accelerated/custom-invisalign-accelerated.vue
@@ -1,12 +1,24 @@
 <template lang="pug" src="./custom-invisalign-accelerated.pug" ></template>
 
 <script>
+import { onScroll } from '@/resources/mixins'
+
 export default {
   props: {
     props: {
       type: Object,
       default: () => ({})
     }
-  }
+  },
+  data () {
+    return {
+      options: {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.5]
+      }
+    }
+  },
+  mixins: [onScroll]
 }
 </script>
