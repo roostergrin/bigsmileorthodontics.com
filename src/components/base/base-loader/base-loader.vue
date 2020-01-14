9f1d1e44ab182f0b617baad959c92b2bce443671
Carlynn <carlynn.espinoza@gmail.com>
2020-01-14 08:53:32 -0800

    updates(base-loader): add logo and update animation

--- src/components/base/base-loader/base-loader.vue
+++ src/components/base/base-loader/base-loader.vue
@@ -1,1 +1,32 @@
 <template lang="pug" src="./base-loader.pug"></template>
+<script>
+export default {
+  props: ['props'],
+  data () {
+    return {
+      loaded: false,
+      animate: false
+    }
+  },
+  created () {
+    document.body.classList.add('body-stop')
+  },
+  mounted () {
+    let isLoading = setInterval(() => {
+      if (!this.$store.state.loading) {
+        setTimeout(() => {
+          console.log('animate')
+          this.animate = true
+        }, 1000)
+        setTimeout(() => {
+          console.log('loaded')
+          this.loaded = true
+          document.body.classList.remove('body-stop')
+        }, 4000)
+        console.log('clearing')
+        clearInterval(isLoading)
+      }
+    }, 100)
+  }
+}
+</script>
