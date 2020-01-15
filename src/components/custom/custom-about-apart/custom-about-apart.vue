f5c27bb3af83c15bd333e9de412a3a89b9c8bb2a
Carlynn <carlynn.espinoza@gmail.com>
2020-01-15 12:50:55 -0800

    updates(custom-about-apart): add animation and left align text

--- src/components/custom/custom-about-apart/custom-about-apart.vue
+++ src/components/custom/custom-about-apart/custom-about-apart.vue
@@ -15,10 +15,23 @@ export default {
       options: {
         root: null,
         rootMargin: '0px',
+        threshold: [0.2]
+      },
+      optionsText: {
+        root: null,
+        rootMargin: '0px',
         threshold: [0.5]
       }
     }
   },
-  mixins: [onScroll]
+  mixins: [onScroll],
+  methods: {
+    onWaypoint ({ going, direction, el }) {
+      if (going === 'in' && direction === 'top') {
+        const htmlItem = el.children[0]
+        htmlItem.classList.add('custom-about-apart__title--active')
+      }
+    }
+  }
 }
 </script>
