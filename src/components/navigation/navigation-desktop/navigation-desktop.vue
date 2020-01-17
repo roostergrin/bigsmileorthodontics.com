db47a9eae20adf0e3da6bdcd6ff91f36dad2194f
Carlynn <carlynn.espinoza@gmail.com>
2020-01-16 16:01:58 -0800

    updates(navigation-desktop): add dental referral modal

--- src/components/navigation/navigation-desktop/navigation-desktop.vue
+++ src/components/navigation/navigation-desktop/navigation-desktop.vue
@@ -1,6 +1,8 @@
 <template lang="pug" src="./navigation-desktop.pug"></template>
 
 <script>
+import FormDentist from 'components/form/form-dentist/form-dentist'
+
 export default {
   props: {
     links: {
@@ -13,10 +15,28 @@ export default {
       type: Boolean
     }
   },
+  data () {
+    return {
+      form: false
+    }
+  },
   computed: {
     contact () {
       return this.$store.state.app['contact-information']
     }
+  },
+  methods: {
+    toggleForm () {
+      this.form = !this.form
+      if (this.form) {
+        document.body.classList.add('body-stop')
+      } else {
+        document.body.classList.remove('body-stop')
+      }
+    }
+  },
+  components: {
+    FormDentist
   }
 }
 </script>
