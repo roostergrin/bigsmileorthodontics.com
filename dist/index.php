d9dbfcc24cd90597d6d7a28f4b60aab9107d4595
Loquen Jones <loquenjones@Loquens-MacBook-Pro.local>
2020-08-30 12:24:10 -0700

    requests(index.php): add ruby script to footer. ticket #41233

--- dist/index.php
+++ dist/index.php
@@ -80,5 +80,22 @@ let prevScrollpos = window.pageYOffset;
     prevScrollpos = currentScrollPos;
   }
 </script>
+<script>
+window.rubyApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};
+(function(){
+  var e="c375af9b-b315-4349-9ac1-468aba92d7fc";
+  var a=false;var t=document.createElement("script");
+  t.async=true;
+  t.type="text/javascript";
+  t.src="https://chatwidget.ruby.com/"+e;
+  document.getElementsByTagName("HEAD").item(0).appendChild(t);
+  t.onreadystatechange=t.onload=function(t){
+    if(!a&&(!this.readyState||this.readyState=="loaded"||this.readyState=="complete")){
+      if(window.RubyChat) window.RubyChat({c:e});
+      a=true
+    }
+  }
+})();
+</script>
 </body>
 </html>
