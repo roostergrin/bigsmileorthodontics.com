4aca1230923087f00822b6caafaf9d11aa14f64d
Skot Walker <skotwalker@Skots-RoosterGrin-Macbook-Pro.local>
2021-07-12 15:43:35 -0700

    Request(custom-3d-tour-iframe.pug/sass):adds header and styling #53324

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,13 +3,13 @@
 <script>
 import HeroHome from 'components/hero/hero-home/hero-home'
 import CustomHomeWelcome from 'components/custom/custom-home-welcome/custom-home-welcome'
-import Custom3DTour from 'components/custom/custom-3d-tour-iframe/custom-3d-tour-iframe'
 import CustomHomeWhy from 'components/custom/custom-home-why/custom-home-why'
 import CustomHomeTestimonials from 'components/custom/custom-home-testimonials/custom-home-testimonials'
 import BlockGradImage from 'components/block/block-grad-image/block-grad-image'
 import CustomHomeBanner from 'components/custom/custom-home-banner/custom-home-banner'
 import CustomHomePatients from 'components/custom/custom-home-patients/custom-home-patients'
 import CustomHomeLifetime from 'components/custom/custom-home-lifetime/custom-home-lifetime'
+import Custom3DTour from 'components/custom/custom-3d-tour-iframe/custom-3d-tour-iframe'
 
 export default {
   computed: {
@@ -20,13 +20,13 @@ export default {
   components: {
     HeroHome,
     CustomHomeWelcome,
-    Custom3DTour,
     CustomHomeWhy,
     CustomHomeTestimonials,
     BlockGradImage,
     CustomHomeBanner,
     CustomHomePatients,
-    CustomHomeLifetime
+    CustomHomeLifetime,
+    Custom3DTour
   }
 }
 </script>
